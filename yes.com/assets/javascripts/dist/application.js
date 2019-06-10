a31945a2d378a73aba419545672b76fcafa6ace0
Pedro Sanches <pedro.sanches@askrobin.com>
2019-06-10 21:37:12 +0800

    update

--- yes.com/assets/javascripts/dist/application.js
+++ yes.com/assets/javascripts/dist/application.js
@@ -367,6 +367,28 @@ App.breakpoint.isMobile = function() {
   return ( App.breakpoint('xs') || App.breakpoint('sm') );
 };
 
+// Bank box scroll
+
+$(function() {
+  var $bankCol = $('#customer__graph__col--right')
+
+  if ( !$bankCol.length ) return;
+
+  var $bankBox = $('#customer__banks-using-yes-container');
+
+  $(window).on('scroll.bankBox', $.throttle(500, function() {
+    if ( App.breakpoint.isMobile() ) return;
+
+    if ( $bankBox.offset().top > $bankCol.offset().top + 300 ) {
+      $bankBox.addClass('active');
+    } else {
+      $bankBox.removeClass('active');
+    }
+  }));
+
+  $(window).trigger('scroll.bankBox');
+});
+
 // Bank Graph
 
 $(function() {
